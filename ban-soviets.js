33f656a14292376dc88dad13d902e0f30454bf4f
orzech123123 <michalorzechowski123@gmail.com>
2023-03-18 14:04:23 +0100

    Added refactored code

--- ban-soviets.js
+++ ban-soviets.js
@@ -1,11 +1,62 @@
-const { spawn } = require('child_process');
-const child = spawn('ls', );
-// use child.stdout.setEncoding('utf8'); if you want text chunks
-child.stdout.on('data', (chunk) => {
-  // data from the standard output is here as buffers
-});
-// since these are streams, you can pipe them elsewhere
-child.stderr.pipe(process.stdout);
-child.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+const { execSync } = require("child_process");
+
+function nextInterval() {
+  setTimeout(function () {
+    let passwordDidNotMatch = execSync(
+      `docker logs 364c44949241 --tail 100 | grep Password\\ did\\ not\\ match`
+    )
+      .toString()
+      .split(/\r?\n/);
+    let couldNotMatchLogin = execSync(
+      `docker logs 364c44949241 --tail 100 | grep Could\\ not\\ find\\ a\\ login\\ matching`
+    )
+      .toString()
+      .split(/\r?\n/);
+
+    let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/g;
+    let ipsToBlockFromDockerLog = [];
+
+    for (const line of passwordDidNotMatch) {
+      var regexResult = line.match(ipRegex);
+      if (!regexResult || regexResult.length < 3) continue;
+      ipsToBlockFromDockerLog.push(regexResult[2]);
+    }
+    for (const line of couldNotMatchLogin) {
+      var regexResult = line.match(ipRegex);
+      if (!regexResult || regexResult.length < 3) continue;
+      ipsToBlockFromDockerLog.push(regexResult[2]);
+    }
+
+    let ipsToBlockDistinct = [...new Set(ipsToBlockFromDockerLog)];
+
+    for (const ip of ipsToBlockDistinct) {
+      let ipTablesForIp = "";
+      try {
+        ipTablesForIp = execSync(`iptables -S | grep ${ip}`);
+      } catch {}
+
+      ipTablesForIp = ipTablesForIp.toString().split(/\r?\n/);
+
+      if (ipTablesForIp != "") {
+        console.log(`${ip} - SKIPPING, already blocked`);
+        continue;
+      }
+
+      let recentFailsCount = ipsToBlockFromDockerLog.filter((log) =>
+        log.includes(ip)
+      ).length;
+
+      if (recentFailsCount < 10) {
+        console.log(`${ip} - SKIPPING, only ${recentFailsCount} recently`);
+        continue;
+      }
+
+      execSync(`iptables --insert DOCKER-USER --destination ${ip} --jump REJECT`)
+      console.log(`${ip} - BLOCKED`);
+    }
+
+    nextInterval();
+  }, 30000);
+}
+
+nextInterval();
